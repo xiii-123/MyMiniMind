1c60b2780d83394a6ff94b1f2224e9f8a3181630
wyh <1058827330@qq.com>
2026-04-02 17:52:32 +0800

    update RMSNorn

--- model/model.py
+++ model/model.py
@@ -1,3 +1,4 @@
+from turtle import forward
 from transformers import PretrainedConfig
 
 
@@ -70,3 +71,17 @@ class MokioMindConfig(PretrainedConfig):
             if self.inference_rope_scaling
             else None
         )
+
+import torch
+import torch.nn as nn
+class RMSNorm(nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-8):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+    
+    def _norm(self, x):
+        return torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps) * x
+    
+    def forward(self, x):
+        return self.weight * self.norm(x.float()).type_as(x)
