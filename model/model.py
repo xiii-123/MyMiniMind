7bb40c699f725883113b2f42ee22044a9e329df1
mac <mac@wyhMacBook-Air.local>
2026-04-06 21:44:06 +0800

    update MoeGate

--- model/model.py
+++ model/model.py
@@ -194,6 +194,73 @@ def repeat_kv(x: torch.Tensor, n_rep: int) -> torch.Tensor:
         .reshape(bs, slen, num_key_value_heads * n_rep, head_dim)
     )
 
+class MoeGate(nn.Module):
+    def __init__(self, config: MyMinimindConfig):
+        super().__init__()
+        self.config = config
+        self.top_k = config.num_experts_per_tok
+        self.n_routed_experts = config.n_routed_experts
+
+        self.scoring_func = config.scoring_func
+        self.alpha = config.aux_loss_alpha
+        self.seq_aux = config.seq_aux
+
+        self.norm_topk_prob = config.norm_topk_prob
+        self.gating_dim = config.hidden_size
+        self.weight = nn.Parameter(
+            torch.empty((self.n_routed_experts, self.gating_dim))
+        )
+        self.gate_proj = nn.Linear(self.gating_dim, self.n_routed_experts, bias=False)
+
+
+    def forward(self, hidden_states):
+        #  init parameters
+        bsz, seq_len, h = hidden_states.shape
+        hidden_states = torch.reshape(hidden_states, (-1, h))
+        logits = self.gate_proj(hidden_states)
+
+        if (self.scoring_func == "softmax"):
+            scores = F.softmax(logits, dim=-1)
+        else:
+            raise NotImplementedError(f"Scoring function {self.scoring_func} not implemented")
+
+        # compute gating scores and norm topk prob if needed
+        topk_weight, topk_idx = torch.topk(scores, self.top_k, dim=-1) + 1e-20
+
+        if self.top_k > 1 and self.norm_topk_prob:
+            denominator = topk_weight.sum(dim=-1, keepdim=True) + 1e-20
+            topk_weight = topk_weight / denominator
+
+        # compute aux loss either sequence-wise or batch-wise
+        if self.training and self.alpha > 0.0:
+            scores_for_aux = scores
+            aux_topk = self.top_k
+            topk_idx_for_aux_loss = topk_idx.view(bsz, -1)
+            if self.seq_aux:
+                scores_for_seq_aux = scores.view(bsz, seq_len, -1)
+                ce = torch.zero(
+                    bsz, self.n_routed_experts, device=hidden_states.device
+                )
+                ce.scatter_add_(
+                    1,
+                    topk_idx_for_aux_loss,
+                    torch.ones(bsz, seq_len * aux_topk, device=hidden_states.device),
+                ).div_(seq_len * aux_topk / self.n_routed_experts)
+                aux_loss = (ce * scores_for_seq_aux.mean(dim=1)).sum(dim=1).mean() * self.alpha
+            else:
+                mask_ce = F.one_hot(
+                    topk_idx_for_aux_loss.view(-1), num_classes=self.n_routed_experts
+                )
+                ce = mask_ce.float().mean(0)
+                Pi = scores_for_aux.mean(0)
+                fi = ce * self.n_routed_experts
+                aux_loss = (fi * Pi).sum() * self.alpha
+        else:
+            aux_loss = scores.new_zeros(1).squeeze()
+        return topk_idx, topk_weight, aux_loss
+                
+
+
 class Attention(nn.Module):
     def __init__(self, config: MyMinimindConfig):
         super().__init__()
